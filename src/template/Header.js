72d68b295de3c204dec4a6f6313841c852f6a1e1
Rogelio Medina <work@Rogelios-MacBook-Pro.local>
2020-12-18 13:30:02 -0600

    initial confit and pages working well

---
+++ src/template/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+  const view =`
+    <div class='Header-main'>
+      <div class='Header-logo'>
+        <h1>
+          <a href="">
+            100tifi.co
+          </a>
+        </h1>
+      </div>
+      <nav class='Header-nav'>
+        <a href="#/about/">about</a>
+      </nav>
+    </div>
+  `;
+  return view;
+};
+
+export default Header;
